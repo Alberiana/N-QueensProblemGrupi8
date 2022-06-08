565848d79fbabb76469035aa728b1f48d1f6913d
donikamj <67651525+donikamj@users.noreply.github.com>
2022-06-08 22:40:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,3 +10,15 @@ def main():
             if(userInput=="exit"):
                 print("Program is stopping...")
                 sys.exit()
+                            if (size <= 3):
+                print("Number of queens should be bigger or equal to 4!\n")
+                continue
+        
+            while True: 
+                chooseAlgorithm=input('Choose the algorithm (BFS - for Breadth First Search or DFS for Depth First Search): ').lower()
+                n_queens = NQueens(size)
+                if(chooseAlgorithm=="bfs"):
+                    while True: 
+                        print_solutions = input('Do you want the solutions to be printed (Y/N): ').lower()
+                        bfs_solutions = n_queens.solve_bfs()
+
