dde135350325d6b4f3c411592ffce2580f28ba3a
Alberiana <atofaj@live.com>
2022-06-08 22:45:15 +0200

    Create n_queens.py

---
+++ n_queens.py
@@ -0,0 +1,16 @@
+from queue import Queue
+
+class NQueens:
+
+    def __init__(self, size):
+        self.size = size
+
+    def solve_dfs(self):
+        if self.size < 1:
+            return []
+        solutions = []
+        stack = [[]]
+        while stack:
+            solution = stack.pop()
+            if self.conflict(solution):
+                continue
